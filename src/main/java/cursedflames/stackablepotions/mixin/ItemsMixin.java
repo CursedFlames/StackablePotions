668556cdfb03852a1f982fbbf67084929d667297
Gu_ZT <administration@ztxy666.cn>
2022-07-07 08:43:09 +0800

    update

--- src/main/java/cursedflames/stackablepotions/mixin/ItemsMixin.java
+++ src/main/java/cursedflames/stackablepotions/mixin/ItemsMixin.java
@@ -12,18 +12,18 @@ public abstract class ItemsMixin {
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;maxCount(I)Lnet/minecraft/item/Item$Settings;", ordinal = 0),
 			slice = @Slice( from = @At(value = "NEW", target = "Lnet/minecraft/item/PotionItem;")))
 	private static int onPotion(int old) {
-		return 16;
+		return 64;
 	}
 	@ModifyArg(method = "<clinit>",
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;maxCount(I)Lnet/minecraft/item/Item$Settings;", ordinal = 0),
 			slice = @Slice( from = @At(value = "NEW", target = "Lnet/minecraft/item/SplashPotionItem;")))
 	private static int onSplashPotion(int old) {
-		return 16;
+		return 64;
 	}
 	@ModifyArg(method = "<clinit>",
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;maxCount(I)Lnet/minecraft/item/Item$Settings;", ordinal = 0),
 			slice = @Slice( from = @At(value = "NEW", target = "Lnet/minecraft/item/LingeringPotionItem;")))
 	private static int onLingeringPotion(int old) {
-		return 16;
+		return 64;
 	}
 }
